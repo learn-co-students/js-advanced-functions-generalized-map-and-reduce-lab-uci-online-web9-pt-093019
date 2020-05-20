ffe9a7d8788c3fa46bdada6eae721819463f72a6
ACAPP-dev <acapp909@gmail.com>
2020-05-20 12:33:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+
+function map(array, funct) {
+    const returnArry = []
+    for (const el of array) {
+        returnArry.push(funct(el))
+    }
+    return returnArry
+}
+
+function reduce(array, funct, startValue=0) {
+    for (const el of array) {
+        startValue = funct(el, startValue)
+    }
+    return startValue
+}
+
+sourceArray = [1, 2, true, "razmatazz"]
+
+console.log(reduce(sourceArray, function(a, memo){ return !!a && !!memo}))
